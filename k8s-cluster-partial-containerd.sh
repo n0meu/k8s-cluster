144c6ea3222b5bc4e51edfdfa75c9b05b0c1f938
n0m <ms@n0m.eu>
2024-01-27 13:54:49 +0100

    FIX insufficient authorizations

--- k8s-cluster-partial-containerd.sh
+++ k8s-cluster-partial-containerd.sh
@@ -20,10 +20,10 @@ sudo sysctl --system
 # - install containerd
 sudo apt update
 sudo apt -y install containerd
-containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 
 # in /etc/containerd/config.toml (currently in line 125) look for the section [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options] and change "SystemdCgroup = false" to "SystemdCgroup = true"
-sed -i '/SystemdCgroup = false/ s/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-sed -i '/sandbox_image = "registry\.k8s\.io\/pause:3\.6"/ s/sandbox_image = "registry\.k8s\.io\/pause:3\.6"/sandbox_image = "registry.k8s.io\/pause:3.9"/' /etc/containerd/config.toml
+sudo sed -i '/SystemdCgroup = false/ s/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo sed -i '/sandbox_image = "registry\.k8s\.io\/pause:3\.6"/ s/sandbox_image = "registry\.k8s\.io\/pause:3\.6"/sandbox_image = "registry.k8s.io\/pause:3.9"/' /etc/containerd/config.toml
 sudo systemctl restart containerd
 sudo systemctl enable containerd
